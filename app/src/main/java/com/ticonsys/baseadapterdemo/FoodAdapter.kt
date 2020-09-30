f97118788617a437722ffaa26accacadecd499dd
Jewel Rana <jewel@ticonsys.com>
2020-09-30 11:52:05 +0600

    update viewBinding for adapter

--- app/src/main/java/com/ticonsys/baseadapterdemo/FoodAdapter.kt
+++ app/src/main/java/com/ticonsys/baseadapterdemo/FoodAdapter.kt
@@ -1,23 +1,23 @@
 package com.ticonsys.baseadapterdemo
 
-import android.view.View
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import com.ticonsys.baseadapter.BaseAdapter
 import com.ticonsys.baseadapterdemo.databinding.SimpleFoodItemBinding
 
 
-class FoodAdapter : BaseAdapter<Food, SimpleFoodItemBinding>(
-    R.layout.simple_food_item
-) {
-    override fun initializeViewBinding(view: View) = SimpleFoodItemBinding.bind(view)
+class FoodAdapter : BaseAdapter<Food, SimpleFoodItemBinding>() {
+
+    override fun initializeViewBinding(
+        layoutInflater: LayoutInflater,
+        parent: ViewGroup
+    ) = SimpleFoodItemBinding.inflate(layoutInflater, parent, false)
 
     override fun initializeDiffItemCallback() = object : DiffUtil.ItemCallback<Food>(){
         override fun areItemsTheSame(oldItem: Food, newItem: Food) = oldItem.id == newItem.id
-
         override fun areContentsTheSame(oldItem: Food, newItem: Food) = oldItem == newItem
-
     }
-
     override fun onBindViewHolder(holder: BaseViewHolder<SimpleFoodItemBinding>, position: Int) {
         val food = differ.currentList[position]
         with(holder.binding){
--- baseadapter/src/main/java/com/ticonsys/baseadapter/BaseAdapter.kt
+++ baseadapter/src/main/java/com/ticonsys/baseadapter/BaseAdapter.kt
@@ -3,27 +3,26 @@ package com.ticonsys.baseadapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseAdapter<T: Any, V: ViewBinding>(
-    @LayoutRes private val resId: Int
-): RecyclerView.Adapter<BaseAdapter.BaseViewHolder<V>>() {
+abstract class BaseAdapter<T: Any, VB: ViewBinding>(
+
+): RecyclerView.Adapter<BaseAdapter.BaseViewHolder<VB>>() {
 
     val differ by lazy {
         AsyncListDiffer(this, initializeDiffItemCallback())
     }
 
-    protected abstract fun initializeViewBinding(view: View): V
+    protected abstract fun initializeViewBinding(layoutInflater: LayoutInflater, parent: ViewGroup): VB
 
     protected abstract fun initializeDiffItemCallback(): DiffUtil.ItemCallback<T>
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<V> {
-        val view = LayoutInflater.from(parent.context).inflate(resId, parent, false)
-        return BaseViewHolder(initializeViewBinding(view))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<VB> {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return BaseViewHolder(initializeViewBinding(layoutInflater, parent))
     }
 
     override fun getItemCount() = differ.currentList.size
@@ -35,7 +34,7 @@ abstract class BaseAdapter<T: Any, V: ViewBinding>(
         this.listener = listener
     }
 
-    class BaseViewHolder<V: ViewBinding>(val binding: V): RecyclerView.ViewHolder(
+    class BaseViewHolder<VB: ViewBinding>(val binding: VB): RecyclerView.ViewHolder(
         binding.root
     )
 
